ce90c7f6012ee6eede637aec54c4bd26f7fcc59f
Amri Nurfalah <85086059+amrinur@users.noreply.github.com>
2023-04-09 17:04:07 +0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,101 +1,114 @@
+/*
+                     KELOMPOK 3
+            Ahmad Jhiro 	(I0722007)
+            Amri Nurfalah	(I0722008)
+            Ardiya Adha F.	(I0722009)
+*/
+
+
+
 #include <iostream>
 #include <cmath>
-#include <vector>
 #include <string>
 #include <algorithm>
-#include <iomanip>
+#include <vector>
+
 using namespace std;
 
+struct Mahasiswa
+{
+    string nama;
+    double average;
+};
 
+//mengurutkan rata rata dari tertinggi ke terendah
+bool compare(Mahasiswa a, Mahasiswa b)
+{
+    return (a.average > b.average);
+}
 
+int main()
+{
+    int bykmhs, bykmatkul;
+    double  sum, nilaiterkecil = TMP_MAX, nilaimatkul, nilaitertinggi = -TMP_MAX;
+    string nama, matkul;
 
-int main() {
-    int mhs, mk;
-    vector<string> nama_mk, nama_mhs;
-    vector<vector<double>> nilai_mk, rata_rata_mhs, nilai_rata_mk;
-    double total = 0.0, rata_rata = 0.0;
-    double nilai_tertinggi = 0.0, nilai_terendah = 100.0;
-
-    cout << "\t\t ------- Program UTS Kelompok 3 --------\n\n" << endl;
-
-    cout << "Masukkan jumlah mahasiswa: ";
-    cin >> mhs;
+    cout << "Masukkan Banyak Mahasiswa: ";
+    cin >> bykmhs;
     cin.ignore();
 
-    cout << "Masukkan jumlah mata kuliah: ";
-    cin >> mk;
+    Mahasiswa* mhs = new Mahasiswa[bykmhs];
+
+    cout << "Masukkan Banyak Matkul: ";
+    cin >> bykmatkul;
     cin.ignore();
 
-    for (int k = 0; k < mk; k++) {
-        string nama;
+    // input mata kuliah
+    vector<string> namamatkul;
+
+    for (int k = 0; k < bykmatkul; k++) {
+        string matkul;
         cout << "Masukkan nama mata kuliah ke- " << k + 1 << ": ";
-        getline(cin, nama);
-        nama_mk.push_back(nama);
+        getline(cin, matkul);
+        namamatkul.push_back(matkul);
     }
 
-    for (int i = 0; i < mhs; i++) {
-        string nama;
-        vector<double> nilai;
-        double sum = 0.0; // reset nilai sum untuk setiap mahasiswa
-        cout << "Masukkan nama mahasiswa ke-" << i + 1 << ": ";
-        getline(cin, nama);
-        nama_mhs.push_back(nama);
+    //input nama mahasiswa
+    for (int j = 0; j < bykmhs; j++) {
+        sum = 0;
+        cout << "\n\t\tMasukkan Nama Mahasiswa ke " << j + 1 << " : ";
+        getline(cin, mhs[j].nama);
 
-        for (int j = 0; j < mk; j++) {
-            double nilai_input;
-            total = 0.0;
-            cout << "Masukkan nilai " << nama_mk[j] << " untuk mahasiswa " << nama << ": ";
-            cin >> nilai_input;
-            nilai.push_back(nilai_input);
-            sum += nilai_input;
+        // input nilai
+        for (int i = 0; i < bykmatkul; i++) {
+            cout << "Nilai " << namamatkul[i] << " : ";
+            cin >> nilaimatkul;
+            cin.ignore();
+            sum += nilaimatkul;
+            mhs[j].average = sum / bykmatkul;
 
-            if (nilai_input > nilai_tertinggi) {
-                nilai_tertinggi = nilai_input;
+            if (nilaimatkul > nilaitertinggi) {
+                nilaitertinggi = nilaimatkul;
             }
-            if (nilai_input < nilai_terendah) {
-                nilai_terendah = nilai_input;
+            if (nilaimatkul < nilaiterkecil) {
+                nilaiterkecil = nilaimatkul;
             }
-        }
-        nilai_mk.push_back(nilai);
-        cin.ignore();
 
-        total += sum;
-
-        rata_rata = static_cast<double>(total) / (mhs * mk);
-
-        double rata = 0.0;
-        for (int j = 0; j < mk; j++) {
-            rata += nilai_mk[i][j];
         }
-        rata /= mk;
 
-        if (rata >= 80) {
-            cout << "Nilai rata-rata " << nama_mhs[i] << " termasuk kategori 'A'" << endl;
+        // hasil inputan
+        cout << "Total Nilai " << mhs[j].nama << " : " << sum << endl;
+        cout << "Rata Rata Nilai " << nama << " : " << mhs[j].average << endl;
+        if (mhs[j].average >= 90) {
+            cout << "Nilai rata-rata " << mhs[j].nama << " termasuk kategori 'A'" << endl;
         }
-        else if (rata >= 70) {
-            cout << "Nilai rata-rata " << nama_mhs[i] << " termasuk kategori 'B'" << endl;
+        else if (mhs[j].average >= 80) {
+            cout << "Nilai rata-rata " << mhs[j].nama << " termasuk kategori 'B'" << endl;
         }
-        else if (rata >= 60) {
-            cout << "Nilai rata-rata " << nama_mhs[i] << " termasuk kategori 'C'" << endl;
+        else if (mhs[j].average >= 70) {
+            cout << "Nilai rata-rata " << mhs[j].nama << " termasuk kategori 'C'" << endl;
         }
         else {
-            cout << "Nilai rata-rata " << nama_mhs[i] << " termasuk kategori 'D'" << endl;
+            cout << "Nilai rata-rata " << mhs[j].nama << " termasuk kategori 'D'" << endl;
         }
 
-        cout << "Nilai rata-rata: " << rata << endl;
-        cout << "Nilai total: " << sum << endl;
-        cout << "Nilai tertinggi: " << nilai_tertinggi << endl;
-        cout << "Nilai terendah: " << nilai_terendah << "\n\n" << endl;
-
-
-
-   
+        // max min nilai
+        cout << "Nilai Terendah " << mhs[j].nama << " : " << nilaiterkecil << endl;
+        cout << "Nilai Tertinggi " << mhs[j].nama << " : " << nilaitertinggi << endl;
 
     }
 
+    // ranking
+    sort(mhs, mhs + bykmhs, compare);
+    cout << "\n\t  PERINGKAT MAHASISWA" << endl;
+    cout << "Rank\t\tNama\t\t\tAverage\n";
 
-
+    for (int j = 0; j < bykmhs; j++)
+    {
+        cout << j + 1 << "\t\t" << mhs[j].nama << "\t\t" << mhs[j].average << endl;
+    }
 
     cin.get();
     return 0;
+
 }
